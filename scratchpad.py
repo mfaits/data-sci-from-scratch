2f76a4ef1439168c62ff24d6e1e085c3c7cb4237
Michelle Faits <mfaits@gmail.com>
2018-10-22 11:51:41 -0500

    first example

--- scratchpad.py
+++ scratchpad.py
@@ -21,5 +21,27 @@ users = [
 { "id": 9, "name": "Klein" }
 ]
 
+friendships = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
+(4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
 
+for user in users:
+    user["friends"] = []
+    
+for i, j in friendships:
+# this works because users[i] is the user whose id is i
+    users[i]["friends"].append(users[j]) # add i as a friend of j
+    users[j]["friends"].append(users[i]) # add j as a friend of i
+
+def number_of_friends(user):
+    """how many friends does _user_ have?"""
+    return len(user["friends"]) # length of friend_ids list
+
+total_connections = sum(number_of_friends(user)
+                        for user in users) # 24
+
+#from __future__ import division # changed this from book code because no longer relevant for python 3
+num_users = len(users) # length of the users list
+avg_connections = total_connections / num_users # 2.4
+
+print(users[3])
 #%%
